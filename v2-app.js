25e66c46a1660f356ec93b13fb51643ba5606ad6
Md. Razu Ahmed <razuahmed.181641@s.pust.ac.bd>
2026-09-23 18:05:06 +0600

    Use custom blue envelope icon for personal email

--- v2-app.js
+++ v2-app.js
@@ -257,7 +257,7 @@
       'orcid': {src:favicon('orcid.org'), cls:'is-orcid'},
       'pust profile': {src:favicon('pust.ac.bd'), cls:'is-pust'},
       'khulna university': {src:favicon('ku.ac.bd'), cls:'is-university'},
-      'personal email': {src:favicon('gmail.com'), cls:'is-personal-email is-gmail'},
+      'personal email': {src:'assets/academic/logos/vendor/personal-email-blue.png', cls:'is-personal-email'},
       'staans lab blog': {src:favicon('blogger.com'), cls:'is-blogger'},
       'ad scientific index': {src:favicon('adscientificindex.com'), cls:'is-adscientific'},
       'banglajol': {src:'assets/academic/journals/ijss/banglajol.png', cls:'is-wide'}
